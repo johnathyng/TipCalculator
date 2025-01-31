000f606a644a92ce178a662f2c4af37030c292e1
Johnathyn Gardner <138516405+johnathyng@users.noreply.github.com>
2025-01-31 03:06:52 -0600

    Added basic button template and altered tip variable names for consistency.

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var billAmount: Double = 0.00
-    @State private var tipAmount: Double = 0
+    @State private var tipPercent: Double = 0
     @State private var numPeople: Double = 1
     
     var body: some View {
@@ -21,7 +21,7 @@ struct ContentView: View {
             // Bill Stack
             VStack {
                 Text ("Bill Amount")
-                    .font(.title)
+                    .font(.headline)
                     .fontWeight(.bold)
                 Text("\(billAmount, specifier: "%.2f")")
                     .font(.title)
@@ -31,24 +31,34 @@ struct ContentView: View {
             
             // Tip Stack
             VStack {
-                Text ("Tip Amount")
-                    .font(.title)
+                Text ("Tip Percentage")
+                    .font(.headline)
                     .fontWeight(.bold)
-                Text("\(tipAmount, specifier: "%.0f") %")
+                Text("\(tipPercent, specifier: "%.0f") %")
                     .font(.title)
                     .fontWeight(.semibold)
-                Slider(value: $tipAmount, in: 0...100)
+                Slider(value: $tipPercent, in: 0...100)
             }
             
             // People Stack
             VStack {
                 Text ("Number of People")
-                    .font(.title)
+                    .font(.headline)
                     .fontWeight(.bold)
                 Text("\(numPeople, specifier: "%.0f")")
                     .font(.title)
                     .fontWeight(.semibold)
                 Slider(value: $numPeople, in: 1...12)
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Calculate")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(10)
             }
         }
         .padding()
