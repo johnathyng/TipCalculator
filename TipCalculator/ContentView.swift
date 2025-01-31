2386d8b2ad4e73e48601127345d67c9f8d5f3455
Johnathyn Gardner <138516405+johnathyng@users.noreply.github.com>
2025-01-31 02:59:15 -0600

    Added basic slider and tip calculator template.

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount: Double = 0.00
+    @State private var tipAmount: Double = 0
+    @State private var numPeople: Double = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+           
+            // Bill Stack
+            VStack {
+                Text ("Bill Amount")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("\(billAmount, specifier: "%.2f")")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Slider(value: $billAmount, in: 0...450)
+            }
+            
+            // Tip Stack
+            VStack {
+                Text ("Tip Amount")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("\(tipAmount, specifier: "%.0f") %")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Slider(value: $tipAmount, in: 0...100)
+            }
+            
+            // People Stack
+            VStack {
+                Text ("Number of People")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("\(numPeople, specifier: "%.0f")")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Slider(value: $numPeople, in: 1...12)
+            }
         }
         .padding()
     }
